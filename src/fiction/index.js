38005800677a8c4733ad3985bdcfe15d047d6a3f
grand3680 <kirill.burtsev07@gmail.com>
2023-09-21 23:49:33 +0300

    add comments

--- src/fiction/index.js
+++ src/fiction/index.js
@@ -3,29 +3,29 @@ import questions from './questions.json';
 import {config} from "./selectors";
 
 
-let currentQuestion = 0; 
+let currentQuestion = 0; // текущий вопрос
 let score = 0; // кол-во правильных ответов
 
 
 // показывает вопросы, берёт currentIndex от вопросов
 function showQuestion() {
-    if (currentQuestion < questions.length) {
-        const question = questions[currentQuestion];
-        config.questionText.textContent = question.question;
-        config.optionsList.innerHTML = "";
-        question.options.forEach((option, index) => {
-            const li = document.createElement("li");
-            li.textContent = option;
-            li.dataset.index = index;
-            config.optionsList.appendChild(li);
-        });
-        currentQuestion++;
-		config.countQuestion.textContent = `${currentQuestion} из ${questions.length} вопросов`;
-    } else {
-        showResult();
-    }
-}
+    // Показывает результат если кол-во вопросов закончилось
+    if (currentQuestion >= questions.length) showResult();
 
+    const question = questions[currentQuestion]; // текущий вопрос
+    config.questionText.textContent = question.question; // заголовок
+    config.optionsList.innerHTML = "";
+
+    // создаёт элемент Li и в Dataser ложит индекс
+    question.options.forEach((option, index) => {
+        const li = document.createElement("li");
+        li.textContent = option;
+        li.dataset.index = index;
+        config.optionsList.appendChild(li);
+    });
+    currentQuestion++;
+    config.countQuestion.textContent = `${currentQuestion} из ${questions.length} вопросов`;
+}
 
 // показывает результат
 function showResult() {
@@ -38,7 +38,6 @@ function showResult() {
 function checkAnswer(selectedIndex) {
     const question = questions[currentQuestion - 1];
     if (question.correctAnswer === selectedIndex) score++;
-    
 }
 
 // ставит ивент слушатель, и изымает dataset.index от элемента на который нажали
